c03f1d202fc1a8bc4f7b3a7fa421ae65d189e66c
wilcoxjoseph11@gmail.com <wilcoxjoseph11@gmail.com>
2026-07-08 15:45:52 -0500

    Refactor server port configuration to use environment variable

--- server/index.js
+++ server/index.js
@@ -113,8 +113,9 @@ app.get("/", (req, res) => {
   res.send("AI Resume API is running");
 });
 
-app.listen(3001, () => {
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
   console.log(
-    "Server running on http://localhost:3001"
+    `Server running on port ${PORT}`
   );
 });
